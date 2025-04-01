a9b7759baab1ffda711f3ef913dd0e5b86ed3f26
Rohith krishnan <unnirohithkrishnan@gmail.com>
2025-04-01 11:55:44 +0530

    minor

---
+++ DSA-week-3/tree/binarytree.js
@@ -0,0 +1,19 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+
+}
+
+const root = new Node(1)
+const value1 = new Node(2)
+const value2 = new Node(3)
+const value3 = new Node(4)
+const value4 = new Node(5)
+
+root.right(value1)
+root.left(value2)
+
+value1.left(value3)
