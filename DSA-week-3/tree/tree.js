83499ca3241bd6b330f2f5b61481ed2859092da5
Rohith krishnan <unnirohithkrishnan@gmail.com>
2025-04-01 10:41:27 +0530

    tree

---
+++ DSA-week-3/tree/tree.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.children = []
+    }
+
+    addchild(child){
+        this.children.push(child)
+    }
+}
+
+const tree = new Node("root")
+const child1 = new Node("child1")
+const child2 = new Node("child2")
+
+tree.addchild(child1)
+tree.addchild(child2)
+
+const grandchild1 = new Node("grandchild1")
+const grandchild2 = new Node("grandchild2")
+
+child1.addchild(grandchild1)
+child1.addchild(grandchild2)
+
+function traverse(node){
+    if(!node) return
+    console.log(node.value)
+
+    for(let child of node.children){
+        traverse(child)
+    }
+}
+
+traverse(tree)
