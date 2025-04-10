164bdf82f51a749241a434a2223b52ddcf30c68b
Rohith krishnan <rohithkrishnan997@gmail.com>
2025-04-10 21:24:44 +0530

    DSA

--- DSA-week-3/trie/trie.js
+++ DSA-week-3/trie/trie.js
@@ -1,136 +1,74 @@
-class TrieNode{
-    constructor(){
-        this.children=new Array(26).fill(null)
-        this.endOfWord=false
+
+class Node {
+    constructor() {
+        this.child = {}
+        this.isEnd = false
     }
-  }
-  
-  class Trie{
-    constructor(){
-        this.root=new TrieNode()
+}
+class Trie {
+    constructor() {
+        this.root = new Node()
     }
-    insert(word){
-        let node=this.root
-        for(let char of word){        
-            let index=this.charCode(char)
-            if(node.children[index]===null){
-                node.children[index]=new TrieNode()
+
+    insert(word) {
+        let node = this.root
+        for (let char of word) {
+            if (!node.child[char]) {
+                node.child[char] = new Node()
             }
-            node=node.children[index]
+            node = node.child[char]
         }
-       node.endOfWord=true
+        node.isEnd = true
     }
-    charCode(char){
-        return char.charCodeAt(0)-'a'.charCodeAt(0)
+
+    search(word) {
+        let node = this.root
+        for (let char of word) {
+            if (!node.child[char]) return false
+            node = node.child[char]
+        }
+        return node.isEnd
     }
-    search(word){
-        let node=this.root
-        for(let char of word){
-            let index=this.charCode(char)
-            if(node.children[index]===null){
-                return false
-            }
-            node=node.children[index]
+
+    startwith(prifix) {
+        let node = this.root
+        for (let char of prifix) {
+            if (!node.child[char]) return false
+            node = node.child[char]
         }
-        return node.endOfWord
+        return true
     }
-    DFS(node,word,result){
-        if(node.endOfWord){
-            result.push(word)
+
+    autocomplete(prefix) {
+        let node = this.root
+        let result = []
+        for (let char of prefix) {
+            if (!node.child[char]) return result
+            node = node.child[char]
         }
-        for(let i=0;i<26;i++){
-            if(node.children[i]){
-                const char=String.fromCharCode(i + 'a'.charCodeAt(0))
-                this.DFS(node.children[i],word+char,result)
-            } 
-        }
+        this.dfs(node, prefix, result)
         return result
     }
-    AutoComplition(prefix){
-        let node=this.root
-        const result=[]
-        for(const char of prefix){
-            const index=this.charCode(char)
-            if(!node.children[index]) return result
-            node=node.children[index]
+
+    dfs(node, prefix, result){
+        if (node.isEnd) {
+         result.push(word)
         }
-        this.DFS(node,prefix,result)
-        return result
-    }
-  }
-  
-  const trie = new Trie();
-  trie.insert('apple');
-  trie.insert('apps');
-  trie.insert('apex');
-  trie.insert('bat');
-  trie.insert('cat');
-  console.log(trie.AutoComplition('a'));
-  console.log(trie.AutoComplition('ap')); 
-  console.log(trie.AutoComplition('bat')); 
-  console.log(trie.AutoComplition('c'));  
-
-  class TrieNode{
-    constructor(){
-        this.children= new Array(26).fill(null)
-        this.EndofWord = false
-    }
-  }
-
-  class Trie{
-    constructor(){
-        this.root = new TrieNode()
-    }
-insert(word){
-    let node =this.root
-    for(let char of word){
-        let index = this.charCode(char)
-        if(node.children[index]===null){
-            node.children[index]= new TrieNode()
-        }
-        node = node.children[index]
-    }
-    node.endOfWord=true
-}
-charCode(char){
-    return char.charCodeAt(0)-'a'.charCodeAt(0)
-}
-
-search(word){
-    let node = this.root
-    for(let char of word){
-        let index=this.charCode(char)
-        if(node.children[index]===null){
-            return false
-        }
-        node = node.children[index]
-    }
-    return node.endOfWord
-}
-
-DFS(node,word,result){
-    if(node.endOfWord){
-        result.push(word)
-    }
-    for(let i=0 ; i<26 ;i++){
-        if(node.children[i]){
-    const char = String.fromCharCode(i+'a'.charCodeAt(0))
-    this.DFS(node.children[i],word+char,result)
+        for (let char in node.child){
+            this.dfs(node.child[char], prefix + char, result)
         }
     }
-    return result
 }
 
-AutoComplition(prefix){
-    let node = this.root
-    const result=[]
-    for(const char of prefix){
-        const index = this.charCode(char)
-        if(!node.children[index])return result
-        node = node.children[index]
-    }
-    this.DFS(node,prefix,result)
-    return result
-}
+const trie = new Trie();
+trie.insert("apple");
+trie.insert("app");
+trie.insert("apps");
+trie.insert("apex");
+trie.insert("bat");
+trie.insert("ball");
 
-  }
+console.log(trie.autocomplete("ap")); // ["apple", "app", "apps", "apex"]
+console.log(trie.autocomplete("b"));  // ["bat", "ball"]
+console.log(trie.autocomplete("c"));  // []
+console.log(trie.search('pple'))
