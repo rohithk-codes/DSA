3de5425017278013b719c43d03328cdc44a9d82e
Rohith krishnan <rohithkrishnan997@gmail.com>
2025-07-30 17:21:38 +0530

    updated

---
+++ typescript/index.js
@@ -0,0 +1,3 @@
+let age = 25;
+let  isStudent = true;
+console.log("Age :".concat(age, " Student : ").concat(isStudent));
---
+++ typescript/index.ts
@@ -0,0 +1,14 @@
+
+
+let age : number = 25;
+let isStudent : boolean= true;
+
+console.log(`Age :${age} Student : ${isStudent}`);
+
+
+
+
+
+
+
+
